68841edfadaae19a4334838ee1e32d2a24af56fd
MisterMartin <1703358+MisterMartin@users.noreply.github.com>
2025-07-15 13:52:15 -0600

    Add decoding for multiple RATSReport versions

--- RatsTM.py
+++ RatsTM.py
@@ -1,6 +1,6 @@
 #
-# See StratoCore_RATS/src/StratoRats.h for StratoRATS::RATSReportHeader_t,
-# which describes the bit-packed TM header block.
+# See StratoCore_RATS/src/RATSReport.h which describes the bit-packed 
+# RATSReport header block.
 #
 # See ECUComm/src/ECUComm.h for ECUComm::ECUReport_t, which describes the
 # bit-packed ECU data block.
@@ -10,32 +10,80 @@ import bitstruct
 import xmltodict
 from bitstruct import *
 
+# The RATSReport contains a bit-packed RATSReport header and a series of ECUReports.
+#
+# The RATSReport and ECUReports have revision numbers, which are stored in the first 4 bits of the record.
+
+# The general scheme for unpacking the bit packed data for the RATSReport and ECUReports 
+# is the same, and utilize the bitstruct library.
+#
+# A "*_bits" dictionary is defined for each version of the RATSReport and ECUReport. These
+# correspond to the C structures defined the C++ header files. When a new version is added,
+# add a new entry to the "_bits" dictionary with the bitstruct format string for that version.
+#
+# Similarly, a "*_field_names" dictionary is defined for each version of the RATSReport and ECUReport.
+# These correspond to the field names in the C++ header files. When a new version is added, 
+# add a new entry to the "_field_names" dictionary with the field names for that version.
+#
+# Finally, scaling functions "*_scaled_vars_v<rev>" are defined for each version of the RATSReport and ECUReport.
+# These functions take the raw data unpacked from the bit-packed format and scale it to human-readable units.
+
+
 # The header section of the RATSReport binary payload
 # The header is defined in StratoCore_RATS/src/StratoRats.h as StratoRats::RATSReportHeader_t.
 # The header is unpacked using bitstruct.
-rats_decode_fmt=(
+rats_bits = {
+    0: (
     '>'    # little-endian
-    'u8'   # header_size_bytes (8 bits)
+    'u4'   # The version of the RATS report header.
+    'u4'   # header_size_bytes (4 bits)
     'u16'  # num_ecu_records (16 bits)
     'u16'  # ecu_record_size_bytes (16 bits)
     'u1'   # ecu_pwr_on (1 bit)
     'u13'  # v56 (13 bits)
-)
+   ),
+   1: (
+    '>'    # little-endian
+    'u4'   # The version of the RATS report header.
+    'u8'   # header_size_bytes (4 bits)
+    'u10'  # num_ecu_records (10 bits)
+    'u9'   # ecu_record_size_bytes (9 bits)
+    'u1'   # ecu_pwr_on (1 bit)
+    'u13'  # v56 (13 bits)
+    'u11'  # cpu_temp (11 bits)
+    'u10'  # lora_rssi (10 bits)
+    'u10'  # lora_snr (10 bits)
+   )
+}
 
-rats_field_names = [
+rats_field_names = {
+    0:[
+    'rev',
     'header_size_bytes', 
     'num_ecu_records', 
     'ecu_record_size_bytes', 
     'ecu_pwr_on', 
     'v56'
-]
+    ],
+    1:[
+    'rev',
+    'header_size_bytes',
+    'num_ecu_records',
+    'ecu_record_size_bytes',
+    'ecu_pwr_on',
+    'v56',
+    'cpu_temp',
+    'lora_rssi',
+    'lora_snr'
+    ]
+}
 
 # The ECU data sections of the RATSReport binary payload
 # The ECU data records are bit-packed and vary by version.
 # The version is stored in the first 4 bits of the record.
 # The format is defined in ECUComm/src/ECUComm.h as ECUComm::ECUReport_t.
 # The ECU data records are unpacked using bitstruct.
-ecu_decode_fmt = {
+ecu_bits = {
     1: (
         '>'    # little-endian
         'u4'   # rev (4 bits)
@@ -129,7 +177,7 @@ ecu_decode_fmt = {
 # The names are defined in ECUComm/src/ECUComm.h as ECUComm::ECUReport_t.
 # The names are grouped by ECU record version.
 # The ECU record version is stored in the first 4 bits of the record.
-ecu_param_names = {
+ecu_field_names = {
     1: [
         'rev', 
         'heat_on', 
@@ -212,8 +260,32 @@ ecu_param_names = {
         'cpu_temp'
     ]
 }
+def rats_scaled_vars_v0(raw_vars):
+    """Scale the raw variables from ECU record version 0."""
+    return {
+        'rev': raw_vars['rev'],
+        'header_size_bytes': raw_vars['header_size_bytes'],
+        'num_ecu_records': raw_vars['num_ecu_records'],
+        'ecu_record_size_bytes': raw_vars['ecu_record_size_bytes'],
+        'ecu_pwr_on': bool(raw_vars['ecu_pwr_on']),
+        'v56': raw_vars['v56'] / 100.0
+    }
 
-def scaled_vars_v1(raw_vars):
+def rats_scaled_vars_v1(raw_vars):
+    """Scale the raw variables from ECU record version 1."""
+    return {
+        'rev': raw_vars['rev'],
+        'header_size_bytes': raw_vars['header_size_bytes'],
+        'num_ecu_records': raw_vars['num_ecu_records'],
+        'ecu_record_size_bytes': raw_vars['ecu_record_size_bytes'],
+        'ecu_pwr_on': bool(raw_vars['ecu_pwr_on']),
+        'v56': raw_vars['v56'] / 100.0,
+        'cpu_temp': raw_vars.get('cpu_temp') / 10.0 - 100.0,
+        'lora_rssi': raw_vars.get('lora_rssi') / 10.0 - 100.0,
+        'lora_snr': raw_vars.get('lora_snr') / 10.0 - 70.0
+    }
+
+def ecu_scaled_vars_v1(raw_vars):
     """Scale the raw variables from ECU record version 1."""
     return {
         'rev': raw_vars['rev'],
@@ -242,7 +314,7 @@ def scaled_vars_v1(raw_vars):
         'tsen_pres': raw_vars['tsen_pres']
     }
 
-def scaled_vars_v2(raw_vars):
+def ecu_scaled_vars_v2(raw_vars):
     """Scale the raw variables from ECU record version 2."""
     return {
         'rev': raw_vars['rev'],
@@ -272,7 +344,7 @@ def scaled_vars_v2(raw_vars):
         'tsen_pres': raw_vars['tsen_pres']
     }   
 
-def scaled_vars_v3(raw_vars):   
+def ecu_scaled_vars_v3(raw_vars):   
     """Scale the raw variables from ECU record version 3."""
     return {
         'rev': raw_vars['rev'],
@@ -339,10 +411,22 @@ if __name__ == "__main__":
             print("Binary payload not found")
             sys.exit(1)
         # Unpack the binary header
-        header = unpack_dict(rats_decode_fmt, rats_field_names, all_bytes[bin_start:])
-        header['v56'] = header['v56']/100.0
+        header_ver = bitstruct.unpack('>u4', all_bytes[bin_start:bin_start+1])[0]
+        print(f'RATSReport header version: {header_ver}')
+        if header_ver not in rats_bits:
+            print(f'Unknown RATSReport header version {header_ver}')
+            sys.exit(1)
+        header = unpack_dict(rats_bits[header_ver], rats_field_names[header_ver], all_bytes[bin_start:])
+
+        # Scale them
+        scaling_function = globals().get(f'rats_scaled_vars_v{header_ver}', None)
+        if scaling_function is None:
+            print(f'No scaling function for RATSReport header version {header_ver}')
+            sys.exit(1)
+        scaled_rats_vars = scaling_function(header)
+
         print("----- RATSReport binary header:")
-        for key, value in header.items():
+        for key, value in scaled_rats_vars.items():
             print(f'{key}: {value}')
         print()
         
@@ -366,22 +450,22 @@ if __name__ == "__main__":
         
             # get the ECU record version from the first 4 bits
             ecu_record_ver = bitstruct.unpack('>u4', ecu_record_bytes[:1])[0]
-            if ecu_record_ver not in ecu_decode_fmt:
+            if ecu_record_ver not in ecu_bits:
                 print(f'Unknown ECU record version {ecu_record_ver} at record {record_num}')
                 break
 
             # Unpack the unscaled parameters
-            vars = unpack_dict(ecu_decode_fmt[ecu_record_ver], ecu_param_names[ecu_record_ver], ecu_record_bytes)
+            vars = unpack_dict(ecu_bits[ecu_record_ver], ecu_field_names[ecu_record_ver], ecu_record_bytes)
 
             # Scale them
-            scaling_function = globals().get(f'scaled_vars_v{ecu_record_ver}', None)
+            scaling_function = globals().get(f'ecu_scaled_vars_v{ecu_record_ver}', None)
             if scaling_function is None:
                 print(f'No scaling function for ECU record version {ecu_record_ver} at record {record_num}')
                 sys.exit(1)
-            scaled_vars = scaling_function(vars)
+            scaled_ecu_vars = scaling_function(vars)
 
             print(f'----- ECU record {record_num}:')
-            for key, value in scaled_vars.items():
+            for key, value in scaled_ecu_vars.items():
                 if key in ['tsen_airt', 'tsen_ptemp', 'tsen_pres']:
                     print(f'{key}: 0x{value:06x}')
                 elif key in ['gps_date']:
