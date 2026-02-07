49e95d4b65966f8f263909cb4062c92b27e24deb
sugartechllc <33792125+sugartechllc@users.noreply.github.com>
2026-02-07 07:52:20 -0700

    Move RATSReport processing into a separate module

---
+++ RATSReport.py
@@ -0,0 +1,76 @@
+import sys
+import bitstruct
+from RatsBitDefs import *
+from RatsScaledVars import *
+
+def decode_ratsreport(all_bytes, bin_start, header_only):
+    header_ver = bitstruct.unpack('>u4', all_bytes[bin_start:bin_start+1])[0]
+    print(f'RATSREPORT header version: {header_ver}')
+    if header_ver not in rats_bits:
+        print(f'Unknown RATSREPORT header version {header_ver}')
+        sys.exit(1)
+    header = bitstruct.unpack_dict(rats_bits[header_ver], rats_field_names[header_ver], all_bytes[bin_start:])
+
+    # Scale them
+    scaling_function = globals().get(f'rats_scaled_vars_v{header_ver}', None)
+    if scaling_function is None:
+        print(f'No scaling function for RATSREPORT header version {header_ver}')
+        sys.exit(1)
+    scaled_rats_vars = scaling_function(header)
+
+    print("----- RATSREPORT binary header:")
+    for key, value in scaled_rats_vars.items():
+        print(f'{key}: {value}')
+    print()
+
+    if header_only:
+        return
+
+    # Keep useful header values in variables
+    header_size = int(header['header_size_bytes'])
+    ecu_record_size = int(header['ecu_record_size_bytes'])
+
+    # Space past the header bytes and get the ECU data records
+    ecu_records_bytes = all_bytes[bin_start+header_size:]
+    record_num = 0
+    offset = 0
+
+    # Unpack the ECU data records
+    while offset < len(ecu_records_bytes):
+        if offset+header_size > len(ecu_records_bytes):
+            # We've reached the terminating bytes at the end of the file
+            break
+
+        # Get the next record
+        ecu_record_bytes = ecu_records_bytes[offset:offset+ecu_record_size]
+
+        # get the ECU record version from the first 4 bits
+        ecu_record_ver = bitstruct.unpack('>u4', ecu_record_bytes[:1])[0]
+        if ecu_record_ver not in ecu_bits:
+            print(f'Unknown ECU record version {ecu_record_ver} at record {record_num}')
+            break
+
+        # Unpack the unscaled parameters
+        vars = bitstruct.unpack_dict(ecu_bits[ecu_record_ver], ecu_field_names[ecu_record_ver], ecu_record_bytes)
+
+        # Scale them
+        scaling_function = globals().get(f'ecu_scaled_vars_v{ecu_record_ver}', None)
+        if scaling_function is None:
+            print(f'No scaling function for ECU record version {ecu_record_ver} at record {record_num}')
+            sys.exit(1)
+        scaled_ecu_vars = scaling_function(vars)
+
+        print(f'----- ECU record {record_num}:')
+        for key, value in scaled_ecu_vars.items():
+            if key in ['tsen_airt', 'tsen_ptemp', 'tsen_pres']:
+                print(f'{key}: 0x{value:06x}')
+            elif key in ['gps_date']:
+                print(f'{key}:   {value:06d}')
+            elif key in ['gps_time']:
+                print(f'{key}: {value:08d}')
+            else:
+                print(f'{key}: {value}')
+        print()
+        record_num += 1
+
+        offset += ecu_record_size
--- RatsTM.py
+++ RatsTM.py
@@ -7,13 +7,10 @@
 # 
 
 import sys
-import bitstruct
 import xmltodict
-from bitstruct import unpack_dict
 import argparse
-from RatsBitDefs import *
-from RatsScaledVars import *
 import os
+import RATSReport
 
 # The RATSReport contains a bit-packed RATSReport header and a series of ECUReports.
 #
@@ -125,78 +122,8 @@ def main(args):
                 bin_start = all_bytes.find(b"START")+5
                 if bin_start == -1:
                     print("Binary payload not found")
-                    sys.exit(1)
-                # Unpack the binary header
-                header_ver = bitstruct.unpack('>u4', all_bytes[bin_start:bin_start+1])[0]
-                print(f'RATSREPORT header version: {header_ver}')
-                if header_ver not in rats_bits:
-                    print(f'Unknown RATSREPORT header version {header_ver}')
-                    sys.exit(1)
-                header = unpack_dict(rats_bits[header_ver], rats_field_names[header_ver], all_bytes[bin_start:])
-
-                # Scale them
-                scaling_function = globals().get(f'rats_scaled_vars_v{header_ver}', None)
-                if scaling_function is None:
-                    print(f'No scaling function for RATSREPORT header version {header_ver}')
-                    sys.exit(1)
-                scaled_rats_vars = scaling_function(header)
-
-                print("----- RATSREPORT binary header:")
-                for key, value in scaled_rats_vars.items():
-                    print(f'{key}: {value}')
-                print()
-                
-                if header_only:
                     continue
-
-                # Keep useful header values in variables
-                header_size = int(header['header_size_bytes'])
-                ecu_record_size = int(header['ecu_record_size_bytes'])
-
-                # Space past the header bytes and get the ECU data records
-                ecu_records_bytes = all_bytes[bin_start+header_size:]
-                record_num = 0
-                offset = 0
-
-                # Unpack the ECU data records
-                while offset < len(ecu_records_bytes):
-                    if offset+header_size > len(ecu_records_bytes):
-                        # We've reached the terminating bytes at the end of the file
-                        break
-
-                    # Get the next record
-                    ecu_record_bytes = ecu_records_bytes[offset:offset+ecu_record_size]
-                
-                    # get the ECU record version from the first 4 bits
-                    ecu_record_ver = bitstruct.unpack('>u4', ecu_record_bytes[:1])[0]
-                    if ecu_record_ver not in ecu_bits:
-                        print(f'Unknown ECU record version {ecu_record_ver} at record {record_num}')
-                        break
-
-                    # Unpack the unscaled parameters
-                    vars = unpack_dict(ecu_bits[ecu_record_ver], ecu_field_names[ecu_record_ver], ecu_record_bytes)
-
-                    # Scale them
-                    scaling_function = globals().get(f'ecu_scaled_vars_v{ecu_record_ver}', None)
-                    if scaling_function is None:
-                        print(f'No scaling function for ECU record version {ecu_record_ver} at record {record_num}')
-                        sys.exit(1)
-                    scaled_ecu_vars = scaling_function(vars)
-
-                    print(f'----- ECU record {record_num}:')
-                    for key, value in scaled_ecu_vars.items():
-                        if key in ['tsen_airt', 'tsen_ptemp', 'tsen_pres']:
-                            print(f'{key}: 0x{value:06x}')
-                        elif key in ['gps_date']:
-                            print(f'{key}:   {value:06d}')
-                        elif key in ['gps_time']:
-                            print(f'{key}: {value:08d}')
-                        else:
-                            print(f'{key}: {value}')
-                    print()
-                    record_num += 1
-
-                    offset += ecu_record_size
+                RATSReport.decode_ratsreport(all_bytes, bin_start, header_only)
             else:
                 print("No RATSREPORT section found in TM XML")
 
